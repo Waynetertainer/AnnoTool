76ead3dc2bbe22168a5cfd263ebb6b173cbd0fa5
Joshua <joshua.tobias@games-academy.de>
2021-06-18 16:02:48 +0200

    Added Icon

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("WpfApp1")]
+[assembly: AssemblyTitle("AnnoTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp1")]
+[assembly: AssemblyProduct("AnnoTool")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
